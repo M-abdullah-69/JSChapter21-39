b7698a37b961ad745824ac5812857f8157a1fb18
Abdullah <58034394+M-abdullah-69@users.noreply.github.com>
2020-06-22 04:41:21 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //              ASSIGNMENTS JAVASCRIPT
 
-//                  Chapter 21 - 39
+//                  Chapter 21 - 25
+
 
 // Task 1:  concatenate the string   
 // var first = prompt("Enter First name..","Abdullah");
@@ -180,3 +181,221 @@
 //         alert("There is no occurence(s) of 'the' ")
 //     }
 // ------------------------------------------------------------------------------------------------------------------------------------------
+
+
+//                  Chapter 26 - 30
+
+// Task 1: convert into number roundoff floor ceil
+// var num = +prompt("Enter a positive number",3.45);
+// var roundOff = Math.round(num)
+// var Floor = Math.floor(num)
+// var Ceil = Math.ceil(num)
+// document.write("a) Value: "+num+"<br>"+"b) Round off value: "+ roundOff +"<br>"+ "c) ceil value: " +Ceil+"<br>"+ "d) floor value: " +Floor)
+
+// ------------------------------------------------------------------------------------------------------------------------------------------
+
+// Task 2:  convert into number roundoff floor ceil
+// var num = +prompt("Enter a negative float number",-3.45);
+// var roundOff = Math.round(num)
+// var Floor = Math.floor(num)
+// var Ceil = Math.ceil(num)
+// document.write("a) Value: "+num+"<br>"+"b) Round off value: "+ roundOff +"<br>"+ "c) ceil value: " +Ceil+"<br>"+ "d) floor value: " +Floor)
+
+// ------------------------------------------------------------------------------------------------------------------------------------------
+
+
+// Task 3: absolute value
+// var num = prompt("Enter a number ",-4);
+// var abs = Math.abs(num);
+// alert("Absolute value of "+num+" is "+abs)
+
+// ------------------------------------------------------------------------------------------------------------------------------------------
+
+
+// Task 4: random
+// var dice = Math.ceil(Math.random()*6);
+// alert("Random dice value "+dice);
+
+// ------------------------------------------------------------------------------------------------------------------------------------------
+
+
+// Task 5: random toss
+// var ran = Math.ceil(Math.random()*2);
+// if (ran == 1){
+//     document.write(1+"<br>"+"Random coin value : Tails")
+// }
+// else if (ran == 2){
+//     document.write(2+"<br>"+"Random coin value : Heads")
+// }
+
+// ------------------------------------------------------------------------------------------------------------------------------------------
+
+
+// Task 6: random b/w 1 and 100;
+// var num = Math.ceil(Math.random()*100);
+// document.write("Random number between 1 and 100 : "+num)
+
+// ------------------------------------------------------------------------------------------------------------------------------------------
+
+
+// Task 7: user weight
+// var weight = +prompt("Enter your wight in kilograms");
+// alert("The weight of user is "+weight+" Kilograms")
+
+// ------------------------------------------------------------------------------------------------------------------------------------------
+
+
+// Task 8: random catch
+// var num = +prompt("Enter a number in between 1 to 10..");
+// var rand = Math.ceil(Math.random()*10)
+// if (num === rand){
+//     alert("congratulation you found it..")
+// }
+// else{
+//     alert("Better luck nect time..")
+// }
+// ------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+//                  Chapter 31 - 34
+
+// Task 1:Local time
+// var d = new Date();
+// alert(d)
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------
+
+
+// Task 2:Current month
+// var d = new Date();
+// var words = ['January','February','March','April','May',"June",'July','August','September','October','November','December']
+// var monthinwords = words[d.getMonth()]
+// alert("Current Month "+monthinwords)
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------
+
+
+// Task 3: alert day
+// var date = new Date;
+// var days = ['Sun','Mon','Tue',"Wed",'Thur','Fri','Sat']
+// var day = days[date.getDay()]
+// alert(day)
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------
+
+
+// Task 4: Funny moment
+// var date = new Date;
+// var days = ['Sun','Mon','Tue',"Wed",'Thur','Fri','Sat']
+// var day = days[date.getDay()]
+// if (day == "Sat" | "Sun"){
+//     alert("It's  Fun day")
+// }
+// else{
+//     alert("It's Work day")
+// }
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------
+
+
+// Task 5:days count
+// var date = new Date;
+// var Date = date.getDate()
+// if (Date<15){
+//         alert("First fifteen  days of the month")
+// }
+// else if (Date>15){
+//         alert("Last days of the month")
+// }
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------
+
+
+// Task 6:Dates Setting
+// var D = new Date;
+// var milli = D.getTime() ;
+// var min = Math.round(D.getTime()/(1000 * 60));
+// document.write("Current Date: "+D +"<br>")
+// document.write("Ellapsed milliseconds since January 1, 1970 : "+milli +"<br>")
+// document.write("Ellapsed minutes since January 1, 1970 : "+min +"<br>")
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------
+
+
+// Task 7:Am or Pm
+// var today = new Date();
+//   var day = today.getDay();
+//   var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+//   console.log("Today is : " + daylist[day] + ".");
+//   var hour = today.getHours();
+//   var minute = today.getMinutes();
+//   var second = today.getSeconds();
+//   var prepand = (hour >= 12)? " PM ":" AM ";
+//   hour = (hour >= 12)? hour - 12: hour;
+//   if (hour===0 && prepand===' PM ') 
+//   { 
+//   if (minute===0 && second===0)
+//   { 
+//   hour=12;
+//   prepand=' Noon';
+//   } 
+//   else
+//   { 
+//   hour=12;
+//   prepand=' PM';
+//   } 
+//   } 
+//   if (hour===0 && prepand===' AM ') 
+//   { 
+//   if (minute===0 && second===0)
+//   { 
+//   hour=12;
+//   prepand=' Midnight';
+//   } 
+//   else
+//   { 
+//   hour=12;
+//   prepand=' AM';
+//   } 
+//   } 
+// document.write("Current Time : "+hour + prepand + " : " + minute + " : " + second);
+// ---------------------------------------------------------------------------------------------------------------------------------------------
+
+
+// Task 8:LASTDATE
+// var d = new Date("June 30, 2020 ");
+// document.write(d)
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------
+
+
+// Task 9:
+// ---------------------------------------------------------------------------------------------------------------------------------------------
+
+
+// Task 10:
+// ---------------------------------------------------------------------------------------------------------------------------------------------
+
+
+// Task 11:
+// ---------------------------------------------------------------------------------------------------------------------------------------------
+
+
+// Task 12:
+// ---------------------------------------------------------------------------------------------------------------------------------------------
+
+
+// Task 13:
+// ---------------------------------------------------------------------------------------------------------------------------------------------
+
+
+// Task 14:
+// ---------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+//                  Chapter 35 - 38
+
+
